1c495beb5d99f07b2fc2a916459bdb087bb4016c
kcsic <es-hm06@hybecorp.com>
2025-03-12 12:41:03 +0900

    firebase npm

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUGlCq0kGvLrzTWX1x02ucmZNZhHrzVcA",
+  authDomain: "chang-react-cabbc.firebaseapp.com",
+  projectId: "chang-react-cabbc",
+  storageBucket: "chang-react-cabbc.firebasestorage.app",
+  messagingSenderId: "637810794010",
+  appId: "1:637810794010:web:03bf01a68526789a7c3ba3"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
